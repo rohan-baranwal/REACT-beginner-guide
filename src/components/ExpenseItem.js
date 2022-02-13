9fdd2471d7fdc27de902e9523c05091712552bb3
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-02-13 20:38:10 +0530

    03-34-more-complex-JSX-code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expense Item</h2>;
+  return (
+    <div>
+      <div>13th February 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>₹200</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
