d70bd850da73818fbf17ac2740d13de0686d12cc
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-02-13 20:27:08 +0530

    03-33-building-a-first-custom-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
       <h2>Let's get started</h2>
+      <p>This is also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
