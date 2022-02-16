77e9d3939ccefd625ad4d596ac24558a2f2752dc
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-02-16 23:20:13 +0530

    03-39 Splitting components into multiple components

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,15 @@
+function ExpenseDate(props) {
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const month = props.date.toLocaleString("en-Us", { month: "long" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
