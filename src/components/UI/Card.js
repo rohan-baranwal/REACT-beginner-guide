73a6c457439adce65a75b4dff601ec30667f231a
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-02-17 20:34:36 +0530

    03-44 Alternative function syntax

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Card.css";
 
-function Card(props) {
+const Card = (props) => {
   const styleClasses = `card ${props.className}`;
   return <div className={styleClasses}>{props.children}</div>;
 }
