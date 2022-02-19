838a61c603a5d7fb2a283d3679e93ade0dcc1dae
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-02-19 18:26:48 +0530

    04-58 Adding 2 way binding

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,18 +5,18 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
-  let dated = props.date
+  let dated = props.date;
   const clickHandler = () => {
     setTitle("Updated");
     dated = new Date();
   };
 
   return (
-    <Card className="expense-item">
+    <Card className='expense-item'>
       <ExpenseDate date={dated} />
-      <div className="expense-item__description">
+      <div className='expense-item__description'>
         <h2>{title}</h2>
-        <div className="expense-item__price">₹{props.amount}</div>
+        <div className='expense-item__price'>₹{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Edit title</button>
     </Card>
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -59,36 +59,29 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
     <form onSubmit={sumbitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
         </div>
-        <div className="new-expense__control">
+        <div className='new-expense__control'>
           <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            onChange={amountChangeHandler}
-          />
+          <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler} />
         </div>
-        <div className="new-expense__control">
+        <div className='new-expense__control'>
           <label>Date</label>
-          <input
-            type="date"
-            min="2015-01-01"
-            max="2025-01-01"
-            onChange={dateChangeHandler}
-          />
+          <input type='date' min='2015-01-01' max='2025-01-01' value={enteredDate} onChange={dateChangeHandler} />
         </div>
       </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+      <div className='new-expense__actions'>
+        <button type='submit'>Add Expense</button>
       </div>
     </form>
   );
